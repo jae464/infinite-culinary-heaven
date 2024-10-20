7b95b650c3bcf2aab14797f37d4458d741cb12a5
이민재 <jae464@naver.com>
2024-10-20 18:40:55 +0900

    [BE] feat: 스프링부트 초기 세팅

---
+++ backend/src/main/java/com/culinaryheaven/CulinaryHeavenApplication.java
@@ -0,0 +1,13 @@
+package com.culinaryheaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryHeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinaryHeavenApplication.class, args);
+	}
+
+}
