002819126cde22d13eb622a584cff810b4c35e4f
이민재 <jae464@naver.com>
2024-11-07 22:38:43 +0900

    [BE] feat: 로컬 서버 실행시 주재료, 대회 샘플 데이터 추가

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO topic_ingredient (name, image) VALUES ('두부', 'tofu.jpg');
+INSERT INTO topic_ingredient (name, image) VALUES ('토마토', 'tomato.jpg');
+INSERT INTO topic_ingredient (name, image) VALUES ('오징어', 'squid.jpg');
+INSERT INTO topic_ingredient (name, image) VALUES ('양파', 'onion.jpg');
+
+INSERT INTO contest (description, start_date, end_date, topic_ingredient_id)
+VALUES
+    ('두부 요리 대회', '2024-10-28T00:00:00', '2024-11-03T23:59:59', 1);
+
+INSERT INTO contest (description, start_date, end_date, topic_ingredient_id)
+VALUES
+    ('토마토 요리 대회', '2024-11-4T00:00:00', '2024-11-10T23:59:59', 2);
