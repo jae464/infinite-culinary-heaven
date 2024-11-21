fecc7da6a1949fa4bc6874b72d46c09f5b6d08ef
이민재 <jae464@naver.com>
2024-11-22 01:29:50 +0900

    [BE] fix: sample sql 이미지 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,6 @@
-INSERT INTO topic_ingredient (name, image) VALUES ('두부', '');
-INSERT INTO topic_ingredient (name, image) VALUES ('토마토', '');
-INSERT INTO topic_ingredient (name, image) VALUES ('오징어', '');
-INSERT INTO topic_ingredient (name, image) VALUES ('양파', '');
+INSERT INTO topic_ingredient (name, image) VALUES ('두부', 'tofu_sample.jpeg');
+INSERT INTO topic_ingredient (name, image) VALUES ('토마토', 'tomato_sample.jpeg');
+INSERT INTO topic_ingredient (name, image) VALUES ('감자', 'potato_sample.jpg');
 
 INSERT INTO contest (description, start_date, end_date, topic_ingredient_id)
 VALUES
@@ -13,4 +12,4 @@ VALUES
 
 INSERT INTO contest (description, start_date, end_date, topic_ingredient_id)
 VALUES
-    ('토마토 요리 대회', '2024-11-11T00:00:00', '2024-11-17T23:59:59', 3);
+    ('감자 요리 대회', '2024-11-11T00:00:00', '2024-11-17T23:59:59', 3);
