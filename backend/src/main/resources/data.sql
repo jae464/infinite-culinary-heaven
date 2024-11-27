9c356c6abc7c42cfe7e94fb616be6560bc1c91a9
이민재 <jae464@naver.com>
2024-11-27 22:57:40 +0900

    [BE] fix: sample data sql 수정

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO topic_ingredient (name, image) VALUES ('두부', 'http://localhost:8080/images/tofu_sample.jpeg');
-INSERT INTO topic_ingredient (name, image) VALUES ('토마토', 'http://localhost:8080/images/tomato_sample.jpeg');
-INSERT INTO topic_ingredient (name, image) VALUES ('감자', 'http://localhost:8080/images/potato_sample.jpg');
-INSERT INTO topic_ingredient (name, image) VALUES ('계란', 'http://localhost:8080/images/egg_sample.jpg');
+INSERT INTO topic_ingredient (name, image) VALUES ('두부', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/tofu_sample.jpeg');
+INSERT INTO topic_ingredient (name, image) VALUES ('토마토', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/tomato_sample.jpeg');
+INSERT INTO topic_ingredient (name, image) VALUES ('감자', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/potato_sample.jpg');
+INSERT INTO topic_ingredient (name, image) VALUES ('계란', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/egg_sample.jpg');
 
 INSERT INTO contest (description, start_date, end_date, topic_ingredient_id)
 VALUES
@@ -21,19 +21,19 @@ VALUES
 
 
 INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, contest_id, competition_count, win_count)
-VALUES ('2024-11-22T11:11:11','2024-11-22T11:11:11','휴게소 알감자', '휴게소에서 파는 알감자의 맛을 그대로 재현해봤습니다.','http://localhost:8080/images/recipe_2_sample.jpg', 3, 0, 0);
+VALUES ('2024-11-22T11:11:11','2024-11-22T11:11:11','휴게소 알감자', '휴게소에서 파는 알감자의 맛을 그대로 재현해봤습니다.','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_2_sample.jpg', 3, 0, 0);
 
 INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('감자', '50g', 1);
 INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('소금', '적당히', 1);
 
-INSERT INTO step (description, image, recipe_id) VALUES ('먼저 감자를 삶습니다.', 'http://localhost:8080/images/step_sample.jpg', 1);
-INSERT INTO step (description, image, recipe_id) VALUES ('감자에 양념을 뿌립니다.', 'http://localhost:8080/images/step_2_sample.jpg', 1);
+INSERT INTO step (description, image, recipe_id) VALUES ('먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_sample.jpg', 1);
+INSERT INTO step (description, image, recipe_id) VALUES ('감자에 양념을 뿌립니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_2_sample.jpg', 1);
 
 INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, contest_id, competition_count, win_count)
-VALUES ('2024-11-25T11:11:11','2024-11-25T11:11:11','들기름 계란 후라이', '간단하고 맛있는 들기름 계란후라이','http://localhost:8080/images/recipe_egg_sample.jpg', 4, 0, 0);
+VALUES ('2024-11-25T11:11:11','2024-11-25T11:11:11','들기름 계란 후라이', '간단하고 맛있는 들기름 계란후라이','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_egg_sample.jpg', 4, 0, 0);
 
 INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('계란', '3개', 2);
 INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('소금', '적당히', 2);
 
-INSERT INTO step (description, image, recipe_id) VALUES ('먼저 감자를 삶습니다.', 'http://localhost:8080/images/step_sample.jpg', 2);
-INSERT INTO step (description, image, recipe_id) VALUES ('감자에 양념을 뿌립니다.', 'http://localhost:8080/images/step_2_sample.jpg', 2);
+INSERT INTO step (description, image, recipe_id) VALUES ('먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_sample.jpg', 2);
+INSERT INTO step (description, image, recipe_id) VALUES ('감자에 양념을 뿌립니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_2_sample.jpg', 2);
