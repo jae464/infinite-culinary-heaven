498fb53a74091e91c16b209aeff9fcae6c182643
이민재 <jae464@naver.com>
2024-12-08 23:46:32 +0900

    [BE] feat: 12월 2주차 대회 sql 데이터 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,6 +6,7 @@ INSERT INTO topic_ingredient (name, image) VALUES ('토마토', 'https://culinar
 INSERT INTO topic_ingredient (name, image) VALUES ('감자', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/potato_sample.jpg');
 INSERT INTO topic_ingredient (name, image) VALUES ('계란', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/egg_sample.jpg');
 INSERT INTO topic_ingredient (name, image) VALUES ('오징어', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/squid_sample.jpeg');
+INSERT INTO topic_ingredient (name, image) VALUES ('연어', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/salmon_sample.jpg');
 
 INSERT INTO contest (name, description, start_date, end_date, topic_ingredient_id)
 VALUES
@@ -27,7 +28,12 @@ INSERT INTO contest (name, description, start_date, end_date, topic_ingredient_i
 VALUES
     ('제5회 대회', '오징어 요리 대회', '2024-12-02T00:00:00', '2024-12-08T23:59:59', 5);
 
+INSERT INTO contest (name, description, start_date, end_date, topic_ingredient_id)
+VALUES
+    ('제6회 대회', '연어 요리 대회', '2024-12-09T00:00:00', '2024-12-15T23:59:59', 6);
 
+
+-- 감자 대회 레시피
 INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
 VALUES ('2024-11-22T11:11:11','2024-11-22T11:11:11','휴게소 알감자', '휴게소에서 파는 알감자의 맛을 그대로 재현해봤습니다.','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_2_sample.jpg', 1, 3);
 
@@ -37,6 +43,7 @@ INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('소금', '적당히'
 INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_sample.jpg', 1);
 INSERT INTO step (step, description, image, recipe_id) VALUES (2, '감자에 양념을 뿌립니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_2_sample.jpg', 1);
 
+--  계란 대회 레시피
 INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
 VALUES ('2024-11-25T11:11:11','2024-11-25T11:11:11','들기름 계란 후라이', '간단하고 맛있는 들기름 계란후라이','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_egg_sample.jpg',1, 4);
 
@@ -46,81 +53,24 @@ INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('소금', '적당히'
 INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_sample.jpg', 2);
 INSERT INTO step (step, description, image, recipe_id) VALUES (2, '감자에 양념을 뿌립니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_2_sample.jpg', 2);
 
+-- 오징어 요리 대회
 INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
 VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
 
 INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 3);
 INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 3);
 
--- 페이징 테스트를 위해 다수 레시피 등록
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 4);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 4);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 5);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 5);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 6);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 6);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 7);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 7);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 8);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 8);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 9);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 9);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 10);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 10);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 11);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 11);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 12);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 12);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 13);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 13);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 14);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 14);
--- 
--- INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
--- VALUES ('2024-12-02T11:11:11','2024-12-02T11:11:11','오징어 볶음', '간단하고 맛있는 오징어 볶음','https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg',1, 5);
--- 
--- INSERT INTO step (step, description, image, recipe_id) VALUES (1, '먼저 감자를 삶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_squid_1_sample.jpg', 15);
--- INSERT INTO step (step, description, image, recipe_id) VALUES (2, '오징어를 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_squid_sample.jpg', 15);
+-- 연어 요리 대회 레시피
+INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
+VALUES ('2024-12-09T11:11:11','2024-12-09T11:11:11','허니 연어 스테이크', '달콤한 허니 글레이즈로 만든 연어 스테이크입니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_salmon_sample.jpg', 1, 6);
+
+INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('연어', '200g', 4);
+INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('꿀', '2 큰술', 4);
+INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('버터', '1 큰술', 4);
+INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('간장', '1 큰술', 4);
+INSERT INTO ingredient (name, quantity, recipe_id) VALUES ('마늘', '다진 것 1 큰술', 4);
+
+INSERT INTO step (step, description, image, recipe_id) VALUES (1, '팬에 버터를 녹이고 다진 마늘을 볶습니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_salmon_1_sample.jpeg', 4);
+INSERT INTO step (step, description, image, recipe_id) VALUES (2, '연어를 넣고 한 면씩 노릇하게 구워줍니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_salmon_2_sample.jpeg', 4);
+INSERT INTO step (step, description, image, recipe_id) VALUES (3, '꿀, 간장을 섞어 만든 소스를 팬에 추가하고 연어에 글레이즈를 입힙니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_salmon_3_sample.png', 4);
+INSERT INTO step (step, description, image, recipe_id) VALUES (4, '접시에 연어를 담고 소스를 뿌린 후 서빙합니다.', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_salmon_sample.jpg', 4);
