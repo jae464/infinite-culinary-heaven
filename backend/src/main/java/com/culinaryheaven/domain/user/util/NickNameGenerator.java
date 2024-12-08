a981ac98cd3fe3a09ea0dffd8db1b062893c9e0d
이민재 <jae464@naver.com>
2024-12-08 23:51:39 +0900

    [BE] feat: 닉네임 adjectives, nouns 몇개 더 추가

--- backend/src/main/java/com/culinaryheaven/domain/user/util/NickNameGenerator.java
+++ backend/src/main/java/com/culinaryheaven/domain/user/util/NickNameGenerator.java
@@ -8,11 +8,13 @@ import java.util.concurrent.ThreadLocalRandom;
 public class NickNameGenerator {
 
     private static final List<String> adjectives = List.of(
-            "빠른", "용감한", "슬기로운", "즐거운", "현명한", "강한", "빛나는", "푸른", "붉은", "검은"
+            "빠른", "용감한", "슬기로운", "즐거운", "현명한", "강한", "빛나는", "푸른", "붉은", "검은",
+            "활기찬", "조용한", "차분한", "날렵한", "우아한", "열정적인", "화려한", "고요한", "대담한", "독창적인"
     );
 
     private static final List<String> nouns = List.of(
-            "호랑이", "독수리", "용", "늑대", "사자", "독사", "곰", "여우", "돌고래", "표범"
+            "호랑이", "독수리", "용", "늑대", "사자", "독사", "곰", "여우", "돌고래", "표범",
+            "까마귀", "부엉이", "토끼", "하이에나", "코끼리", "치타", "펭귄", "고래", "판다", "코알라"
     );
 
     public static String generateNickName() {
