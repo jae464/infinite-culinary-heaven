ae37ec96f8b6e13c0b8f55fd052bcaace53fa74b
이민재 <jae464@naver.com>
2024-12-15 19:53:20 +0900

    [BE] feat: docker-compose-monitor 파일 추가 및 slack webhook 추가

--- backend/src/main/java/com/culinaryheaven/global/controller/HealthController.java
+++ backend/src/main/java/com/culinaryheaven/global/controller/HealthController.java
@@ -1,10 +1,19 @@
 package com.culinaryheaven.global.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthController {
+
     @GetMapping("/health")
-    public String health() { return "OK1"; }
+    public String health() {
+        log.error("slack 알림 test");
+        return "OK1";
+    }
 }
