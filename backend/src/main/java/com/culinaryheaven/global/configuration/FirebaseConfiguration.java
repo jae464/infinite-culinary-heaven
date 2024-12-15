858e4a0e11e26ce7dbc35aae2297200da462be49
이민재 <jae464@naver.com>
2024-12-16 02:02:47 +0900

    [BE] test: firebase admin key copy test

--- backend/src/main/java/com/culinaryheaven/global/configuration/FirebaseConfiguration.java
+++ backend/src/main/java/com/culinaryheaven/global/configuration/FirebaseConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.core.io.ResourceLoader;
 import java.io.IOException;
 
 @Configuration
-@Profile("local")
 public class FirebaseConfiguration {
 
     @Value("${firebase.private-key-path}")
