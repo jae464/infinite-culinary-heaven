048f004fdd1b5967182736008cbce0c7f5a9444e
이민재 <jae464@naver.com>
2024-12-22 22:03:37 +0900

    [AND] chore: 불필요한 코드 제거

--- android/data/src/main/java/com/jae464/data/di/RetrofitModule.kt
+++ android/data/src/main/java/com/jae464/data/di/RetrofitModule.kt
@@ -13,7 +13,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
-import java.io.IOException
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
