532ec1f5a28d5dde1fba57e0498044e8afe7ceef
이민재 <jae464@naver.com>
2024-12-23 12:22:27 +0900

    [BE] feat: 12월 4주차 대회 sql 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,6 +15,9 @@ INSERT INTO topic_ingredient (name, image)
 VALUES ('연어', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/salmon_sample.jpg');
 INSERT INTO topic_ingredient (name, image)
 VALUES ('당근', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/carrot_sample.jpg');
+INSERT INTO topic_ingredient (name, image)
+VALUES ('새우', 'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/shrimp_sample.jpg');
+
 
 INSERT INTO contest (name, description, start_date, end_date, topic_ingredient_id)
 VALUES ('제1회 대회', '두부 요리 대회', '2024-11-04T00:00:00', '2024-11-10T23:59:59', 1);
@@ -39,6 +42,10 @@ VALUES ('제6회 연어 요리 대회',
 INSERT INTO contest (name, description, start_date, end_date, topic_ingredient_id)
 VALUES ('제7회 당근 요리 대회', '이번 주 주재료는 당근입니다. 다양하고 창의적인 당근 요리를 기대합니다.',
         '2024-12-16T00:00:00', '2024-12-22T23:59:59', 7);
+
+INSERT INTO contest (name, description, start_date, end_date, topic_ingredient_id)
+VALUES ('제8회 새우 요리 대회', '이번 주 주재료는 새우입니다.',
+        '2024-12-23T00:00:00', '2024-12-29T23:59:59', 8);
 
 
 -- 감자 대회 레시피
@@ -109,3 +116,38 @@ VALUES (3, '꿀, 간장을 섞어 만든 소스를 팬에 추가하고 연어에
 INSERT INTO step (step, description, image, recipe_id)
 VALUES (4, '접시에 연어를 담고 소스를 뿌린 후 서빙합니다.',
         'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_salmon_sample.jpg', 4);
+
+-- 새우 요리 대회 레시피
+INSERT INTO recipe (created_at, modified_at, title, description, thumbnail_image, user_id, contest_id)
+VALUES ('2024-12-23T00:00:01', '2024-12-23T00:00:01', '새우 치즈 버터 구이', '호불호 없는 음식 새우 치즈 버터 구이입니다.',
+        'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/recipe_shrimp_sample.jpg', 1, 8);
+
+INSERT INTO ingredient (name, quantity, recipe_id)
+VALUES ('대하', '500g', 5);
+INSERT INTO ingredient (name, quantity, recipe_id)
+VALUES ('청주', '2 큰술', 5);
+INSERT INTO ingredient (name, quantity, recipe_id)
+VALUES ('피자치즈', '40g', 5);
+INSERT INTO ingredient (name, quantity, recipe_id)
+VALUES ('마요네즈', '4 큰술', 5);
+INSERT INTO ingredient (name, quantity, recipe_id)
+VALUES ('후추', '약간', 5);
+
+INSERT INTO step (step, description, image, recipe_id)
+VALUES (1, '대하의 수염과 다리를 잘라 손질해 주세요. 등을 따라 껍데기에 가위집을 내고 칼집을 넣어 반으로 잘라 주세요.',
+        'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_shrimp_1.jpg', 5);
+INSERT INTO step (step, description, image, recipe_id)
+VALUES (2, '손질한 대하에 청주 2큰술과 후추 약간을 넣고 밑간을해주세요. 밑간한 대하는 키친타월에 올려 물기를 제거해 주세요.',
+        'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_shrimp_2.jpg', 5);
+INSERT INTO step (step, description, image, recipe_id)
+VALUES (3, '볼에 피자치즈, 마요네즈, 다진 마늘, 후춧가루를 넣고 섞은 후 짤 주머니에 넣어 준비해 주세요.',
+        'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_shrimp_3.jpg', 5);
+INSERT INTO step (step, description, image, recipe_id)
+VALUES (4, '법랑 접시에 대하를 동그랗게 펼친 후 만들어둔 소스를 짜서 올려주세요.',
+        'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_shrimp_4.jpg', 5);
+INSERT INTO step (step, description, image, recipe_id)
+VALUES (5, '법랑 접시를 3단에 넣은 후 광파오븐 수동 요리 <구이>에서 17분간 구워주세요.',
+        'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_shrimp_5.jpg', 5);
+INSERT INTO step (step, description, image, recipe_id)
+VALUES (6, '완성된 대하 치즈구이를 접시에 담아 레몬, 파슬리 등을 올려 맛있게 즐겨주세요.',
+        'https://culinary-heaven.s3.ap-northeast-2.amazonaws.com/image/step_shrimp_6.jpg', 5);
