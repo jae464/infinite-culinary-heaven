1fbb67a893c6a7fb170f2b450654ade3c4bd1d9f
이민재 <jae464@naver.com>
2025-02-02 22:11:40 +0900

    [AND] design: ConfirmDialog 컴포넌트 생성

---
+++ android/presentation/src/main/java/com/jae464/presentation/component/ConfirmDialog.kt
@@ -0,0 +1,100 @@
+package com.jae464.presentation.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.jae464.presentation.ui.theme.Gray20
+import com.jae464.presentation.ui.theme.Green10
+
+@Composable
+fun ConfirmDialog(
+    title: String,
+    content: String,
+    modifier: Modifier = Modifier,
+    cancelText: String,
+    confirmText: String,
+    onDismissRequest: () -> Unit,
+    onConfirm: () -> Unit,
+    onCancel: () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        Surface(
+            modifier = modifier.width(350.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    text = title
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = content)
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Button(
+                        onClick = onCancel,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 8.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Gray20,
+                            contentColor = Color.Black
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                    ) {
+                        Text(text = cancelText)
+                    }
+
+                    Button(
+                        onClick = onConfirm,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 8.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Green10,
+                            contentColor = Color.White
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                    ) {
+                        Text(text = confirmText)
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ConfirmDialogPreview() {
+    ConfirmDialog(
+        title = "정말 삭제할까요?",
+        content = "삭제되면 되돌릴 수 없어요",
+        confirmText = "확인",
+        onDismissRequest = {},
+        onConfirm = {},
+        onCancel = {},
+        cancelText = "취소",
+    )
+}
--- android/presentation/src/main/java/com/jae464/presentation/component/ImageDetailDialog.kt
+++ android/presentation/src/main/java/com/jae464/presentation/component/ImageDetailDialog.kt
@@ -2,7 +2,6 @@ package com.jae464.presentation.component
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -10,9 +9,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
