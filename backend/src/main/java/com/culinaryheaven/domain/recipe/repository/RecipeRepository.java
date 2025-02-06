bb9f85d5bd5178ea00016a93d60f34fb1b4cd624
이민재 <jae464@naver.com>
2025-02-07 00:43:21 +0900

    [BE] feat: recipe 조회 fetch join 적용. batch_fetch_size 적용

--- backend/src/main/java/com/culinaryheaven/domain/recipe/repository/RecipeRepository.java
+++ backend/src/main/java/com/culinaryheaven/domain/recipe/repository/RecipeRepository.java
@@ -6,9 +6,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    @Query(value = "select distinct r from Recipe r " +
+            "join fetch r.user  u " +
+            "join fetch r.contest c join fetch c.topicIngredient ct",
+            countQuery = "select count(r) from Recipe r")
     Page<Recipe> findAll(Pageable pageable);
+    @Query(value = "select distinct r from Recipe r " +
+            "join fetch r.user  u " +
+            "join fetch r.contest c join fetch c.topicIngredient ct where r.contest.id = :contestId",
+            countQuery = "select count(r) from Recipe r where r.contest.id = :contestId")
     Page<Recipe> findAllByContestId(Pageable pageable, Long contestId);
     Page<Recipe> findByTitleContainingIgnoreCase(Pageable pageable, String keyword);
     Page<Recipe> findAllByUserId(Pageable pageable, Long userId);
