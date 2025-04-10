fa85dd20171adceb46bd1fbb2c7cf5ac60ed841e
이민재 <jae464@naver.com>
2025-04-10 13:03:38 +0900

    [BE] fix: EndTimeLimit 어노테이션에 메시지 추가

--- backend/src/main/java/com/culinaryheaven/global/annotation/EndTimeLimit.java
+++ backend/src/main/java/com/culinaryheaven/global/annotation/EndTimeLimit.java
@@ -2,6 +2,7 @@ package com.culinaryheaven.global.annotation;
 
 import com.culinaryheaven.global.annotation.validator.EndTimeValidator;
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,4 +13,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = EndTimeValidator.class)
 public @interface EndTimeLimit {
+    String message() default "대회 끝나는 시간은 일요일 11시 59분 59초이어야 합니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
